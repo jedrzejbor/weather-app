b6eeb42df695583f68b3ca2ce7501202b1c9b86e
Jędrzej <jedrzej.borakiewicz@gmail.com>
2019-09-10 20:59:53 +0200

    Updates

---
+++ precache-manifest.187cedfb6f8b7547ccab6b39838026c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1aacd0d320538f8c2dfff4c1f2ed774c",
+    "url": "/weather-app/static/media/sky-690293_1920.1aacd0d3.jpg"
+  },
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "725b373776e135958c6b",
+    "url": "/weather-app/static/js/main.725b3737.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/weather-app/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "725b373776e135958c6b",
+    "url": "/weather-app/static/css/main.53a96137.chunk.css"
+  },
+  {
+    "revision": "a14cdd492908ed560475e0d8606fecdc",
+    "url": "/weather-app/index.html"
+  }
+];
